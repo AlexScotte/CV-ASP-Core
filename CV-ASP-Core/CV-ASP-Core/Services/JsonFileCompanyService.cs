4b78bc72d63680824a54f964b0997c0d2cec5edd
ascotte <alexandre.scotte@resolving.com>
2020-07-02 12:18:39 +0200

    - Adding of first service to manage companies data

---
+++ CV-ASP-Core/CV-ASP-Core/Services/JsonFileCompanyService.cs
@@ -0,0 +1,54 @@
+﻿using CV_ASP_Core.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CV_ASP_Core.Services {
+    public class JsonFileCompanyService {
+        public JsonFileCompanyService(IWebHostEnvironment webHostEnvironment) {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "cv-data.json"); }
+        }
+
+        public IEnumerable<Company> GetCompanies() {
+            using (var jsonFileReader = File.OpenText(JsonFileName)) {
+                return JsonSerializer.Deserialize<Company[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        //public void AddRating(string productId, int rating) {
+        //    var products = GetProducts();
+
+        //    if (products.First(x => x.Id == productId).Ratings == null) {
+        //        products.First(x => x.Id == productId).Ratings = new int[] { rating };
+        //    } else {
+        //        var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+        //        ratings.Add(rating);
+        //        products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+        //    }
+
+        //    using (var outputStream = File.OpenWrite(JsonFileName)) {
+        //        JsonSerializer.Serialize<IEnumerable<Product>>(
+        //            new Utf8JsonWriter(outputStream, new JsonWriterOptions {
+        //                SkipValidation = true,
+        //                Indented = true
+        //            }),
+        //            products
+        //        );
+        //    }
+        //}
+    }
+
+}
--- CV-ASP-Core/CV-ASP-Core/Startup.cs
+++ CV-ASP-Core/CV-ASP-Core/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CV_ASP_Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -20,6 +21,7 @@ namespace CV_ASP_Core {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
+            services.AddTransient<JsonFileCompanyService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

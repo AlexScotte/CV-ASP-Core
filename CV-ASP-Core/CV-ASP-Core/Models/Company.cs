52c19670a02de6cbe07863ee1ea6d49219a1f887
ascotte <alexandre.scotte@resolving.com>
2020-07-02 12:15:32 +0200

    Adding of first model class and json mocq file

---
+++ CV-ASP-Core/CV-ASP-Core/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CV_ASP_Core.Models {
+    public class Company {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Town { get; set; }
+        public string Job { get; set; }
+        public string Department { get; set; }
+        public string DateStart { get; set; }
+        public string DateEnd { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Company>(this);
+
+    }
+}

9ec0c4a8358ca2743b6817b9f9c4ddc6390d5c53
ascotte <alexandre.scotte@resolving.com>
2020-09-18 10:17:47 +0200

    Creating profile page model

--- CV-ASP-Core/CV-ASP-Core/Pages/MyProfile.cshtml.cs
+++ CV-ASP-Core/CV-ASP-Core/Pages/MyProfile.cshtml.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CV_ASP_Core.Models;
+using CV_ASP_Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace CV_ASP_Core.Pages
-{
-    public class ProfileModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace CV_ASP_Core.Pages {
+    public class ProfileModel : PageModel {
 
+        private readonly ILogger<ProfileModel> _logger;
+        private readonly JsonFileProfileService _profileService;
+        public Profile Profile { get; set; }
+        public IEnumerable<Skill> Skills { get; set; }
+
+        public ProfileModel(ILogger<ProfileModel> logger, JsonFileProfileService profileService) {
+            _logger = logger;
+            _profileService = profileService;
+        }
+
+        public virtual void OnGet() {
+
+            Profile = _profileService.GetProfile();
+            Skills = _profileService.GetDistinctSkills();
         }
     }
 }
